726cd712a94787a38cdf7a29e88be1e63c80cd66
欧子 <ouzi7997@163.com>
2021-03-05 23:59:31 +0800

    stripe

---
+++ stripe/create-checkout-session.php
@@ -0,0 +1,31 @@
+<?php
+
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
+
+require '../vendor/autoload.php';
+Stripe::setApiKey('sk_test_51IRXGkJze1kcJ9654BaktLfKCLXJoy6rqybKysznoqRF25rnVuIHEOLTEnQ86Lc46fdC4zNgplNLlrOZfAmujP6Y00scJvkehD');
+
+header('Content-Type: application/json');
+
+$YOUR_DOMAIN = 'http://demo.test';
+
+$checkout_session = Session::create([
+    'payment_method_types' => ['card'],
+    'line_items' => [[
+        'price_data' => [
+            'currency' => 'usd',
+            'unit_amount' => 2000,
+            'product_data' => [
+                'name' => 'Stubborn Attachments',
+                'images' => ["https://i.imgur.com/EHyR2nP.png"],
+            ],
+        ],
+        'quantity' => 1,
+    ]],
+    'mode' => 'payment',
+    'success_url' => $YOUR_DOMAIN . '/success.html',
+    'cancel_url' => $YOUR_DOMAIN . '/cancel.html',
+]);
+
+echo json_encode(['id' => $checkout_session->id]);
